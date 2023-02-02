b2dae1758ca9f57de91dc1eeef378929268fbbaf
edelisec <edelisec21@gmail.com>
2023-02-02 14:04:18 -0300

    Actualização do projeto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import validator from './validator.js';
 
+const cNumber = document.getElementById('card-number');
+const result = document.getElementById('result');
+const form = document.getElementById("validation-form");
 
 
-console.log(validator);
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const ccNumber = cNumber.value;
+  if (validator.isValid(ccNumber)) {
+    result.innerHTML = "Número de cartão válido!";
+  } else {
+    result.innerHTML = "Número de cartão inválido!";
+  }
+  cNumber.value = validator.maskify(cNumber.value);
+});
 
--- src/validator.js
+++ src/validator.js
@@ -1,2 +1,31 @@
+const validator = {
+  isValid: function (creditCardNumber) { 
+    // Invertir el número de la tarjeta de crédito
+    console.log(creditCardNumber);
+    creditCardNumber = creditCardNumber.split("").reverse().join("");
+    // Aplicar el algoritmo de validación
+    let sum = 0;
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      let digit = parseInt(creditCardNumber[i]);
+      if (i % 2 !== 0) {
+        digit *= 2;
+        if (digit > 9) {
+          digit = digit.toString().split("").reduce((a, b) => parseInt(a) + parseInt(b));
+        }
+      }
+      sum += digit;
+    }
+    console.log(sum % 10 === 0);
+    return sum % 10 === 0;
+  },
+  maskify: function (creditCardNumber) {
+    let maskNumber = "";
+    for (let i = 0; i < creditCardNumber.length - 4; i++) {
+      maskNumber += "#";
+    }
+    maskNumber += creditCardNumber.substr(-4);
+    return maskNumber
+  }
+};
 
 export default validator; 
